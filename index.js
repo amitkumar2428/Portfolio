9d74ca4c70e9832079545f1ee940c2a3b672024c
Amit <ak6760069@gmail.com>
2024-07-02 19:25:37 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const burger=document.querySelector('.burger');
-const navLinks=document.querySelector('.nav-links');
-burger.addEventListener('click',()=>{
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
@@ -10,47 +10,47 @@ var typed = new Typed('#element', {
 });
 
 
-function Submit(){
-   
-    let nameMessage=document.getElementById('nameError');
-    let emailMessage=document.getElementById('emailError');
-    let phoneMessage=document.getElementById('phoneError');
+function Submit() {
 
-    let name=document.getElementById('name').value;
-    let email=document.getElementById('email').value;
-    let phone=document.getElementById('phone').value;
+    let nameMessage = document.getElementById('nameError');
+    let emailMessage = document.getElementById('emailError');
+    let phoneMessage = document.getElementById('phoneError');
 
-    let isValid=true;
-    if(name===''){
-        nameMessage.textContent='Fill the above box';
-        
-        isValid=false;
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+
+    let isValid = true;
+    if (name === '') {
+        nameMessage.textContent = 'Fill the above box';
+
+        isValid = false;
     }
-    if(email===''){
-        emailMessage.textContent='Fill the above box';
-       
-        isValid=false;
+    if (email === '') {
+        emailMessage.textContent = 'Fill the above box';
+
+        isValid = false;
     }
-    if(phone===''){
-        phoneMessage.textContent='Fill the above box';
-       
-        isValid=false;
+    if (phone === '') {
+        phoneMessage.textContent = 'Fill the above box';
+
+        isValid = false;
     }
-    if(isValid){
-        document.getElementById('ThankYou').textContent='Thank you for your submission';
-        nameMessage.textContent='';
-        emailMessage.textContent='';
-        phoneMessage.textContent='';
-        let inputs=document.querySelectorAll('#contact input, #contact textarea, #contact button');
-        inputs.forEach(input=>input.disabled=true);
+    if (isValid) {
+        document.getElementById('ThankYou').textContent = 'Thank you for your submission';
+        nameMessage.textContent = '';
+        emailMessage.textContent = '';
+        phoneMessage.textContent = '';
+        let inputs = document.querySelectorAll('#contact input, #contact textarea, #contact button');
+        inputs.forEach(input => input.disabled = true);
     }
-    document.getElementById('contact').addEventListener('submit',function(event){
+    document.getElementById('contact').addEventListener('submit', function (event) {
         event.preventDefault();
     })
-    const firstInvalidControl=document.querySelector('.error');
-    if(firstInvalidControl){
+    const firstInvalidControl = document.querySelector('.error');
+    if (firstInvalidControl) {
         firstInvalidControl.scrollIntoView({
-            behavior:'smooth'
+            behavior: 'smooth'
         });
     }
 };
